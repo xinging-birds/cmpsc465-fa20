f8df348bbc6462733a23d2b260e3464b85a27c69
Justin Huang <xinglongjh@gmail.com>
2020-09-05 13:36:10 -0400

    Assignment1 (#1)
    * Change problem 1 to account for a-is-empty-before-b case
    
    * Fixed integer comparison in problem 1

--- assignment1/problem1/solution.py
+++ assignment1/problem1/solution.py
@@ -12,15 +12,20 @@ elif n == '0':
     print(list_1)
 else:
     a = list_1[2:].split()
+    print(a)
     b = list_2[2:].split()
-
+    print(b)
     c = [str(int(m) + int(n))]
 
-    while len(a) or len(b):
-        if (len(a) and not len(b)) or a[0] < b[0]:
+    while a or b:
+        if not b:
             c.append(a.pop(0))
-        else:
+        elif not a:
             c.append(b.pop(0))
-
+        else:
+            if int(a[0]) < int(b[0]):
+                c.append(a.pop(0))
+            else:
+                c.append(b.pop(0))
 
     print(' '.join(c))
